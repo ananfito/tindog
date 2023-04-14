73d80cb64292d1cd683e6c16b40daf4a367b86f3
Anthony Nanfito <awn8472@gmail.com>
2023-04-14 16:29:19 +0700

    add functions and class to render dog avatar

--- Dog.js
+++ Dog.js
@@ -4,7 +4,23 @@ class Dog {
     }
 
     // method for getting avatar HTML
-    
+    getAvatarHtml() {
+        const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this
+
+        return `
+            <img id="img-avatar" src="${avatar}" alt="the dog's avatar image">
+            <div class="avatar-content" id="avatar-content">
+                <p class="avatar-info">${name}, ${age}</p>
+                <p class="avatar-msg">How you doin?</p>
+            </div>
+            <div class="badge hidden" id="badge-reject">
+                <img src="./images/badge-nope.png" alt="badge">
+            </div>
+            <div class="badge hidden" id="badge-accept">
+                <img src="./images/badge-like.png" alt="badge">
+            </div>
+            `
+    }
 }
 
 export default Dog
--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-import dog from "./data.js"
+import dogs from "./data.js"
 import Dog from "./Dog.js";
 
+// const rex = new Dog(dogs[0])
+// const bella = new Dog(dogs[1])
+// const teddy = new Dog(dogs[2])
+
+let dogsArray = [0, 1, 2]
+
+// get new dog avatar
+function getNewDogAvatar() {
+    const nextDogAvatar = dogs[dogsArray.shift()]
+    return nextDogAvatar ? new Dog(nextDogAvatar) : {}
+}
+
+// render function
+function render() {
+    document.getElementById('img-avatar').innerHTML = getNewDogAvatar().getAvatarHtml()
+}
+
 // event listener for reject/accept btns
+document.getElementById('btn-reject').addEventListener('click', () => {
+    document.getElementById('badge-reject').classList.toggle('hidden')
+    setTimeout(render, 1500)
+})
 
-// render function 
-// call method for dog avatar
-
-// create new dog avatar
-
-// ?: Function for getting next dog avatar or would it be method? or would it be using setTimeout?
+document.getElementById('btn-accept').addEventListener('click', () => {
+    document.getElementById('badge-accept').classList.toggle('hidden')
+    setTimeout(render, 1500)
+})
 
 // call render function
+render()
